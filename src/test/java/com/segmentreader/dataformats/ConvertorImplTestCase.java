f528cc485051a17cc6821239981d8a7c8acb32d7
Ramiz <abadulla@live.ru>
2016-02-04 18:01:20 +0200

    added new test case

--- src/test/java/com/segmentreader/dataformats/ConvertorImplTestCase.java
+++ src/test/java/com/segmentreader/dataformats/ConvertorImplTestCase.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,12 +16,14 @@ import com.segmentreader.mapreduce.UserModCommand;
 
 public class ConvertorImplTestCase {
 
+    private static final String inputDate = "2011-12-03T10:15:30+01:00";
+    
     @Test
     public void testConvertorWithValidInput() throws IOException {
-       String input = "2011-12-03T10:15:30+01:00,14,add,generatedlink,closedtab";
+       String input = inputDate+",14,add,generatedlink,closedtab";
        Convertor convertor = new ConvertorImpl();
        List<String> expectedSegments = Arrays.asList(input.split(",")).subList(3, 5);
-       Instant timestamp = Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse("2011-12-03T10:15:30+01:00"));
+       Instant timestamp = Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(inputDate));
        UserModCommand expected = new UserModCommand(timestamp, "14","add",expectedSegments);
        UserModCommand umc = convertor.convert(input);
        
@@ -29,10 +32,22 @@ public class ConvertorImplTestCase {
     
     @Test(expected=InvalidArgumentException
             .class)
-    public void testConvertorWithInvalidInput() throws IOException, InvalidArgumentException {
-       String input = Instant.EPOCH.toString()+",14,add";
+    public void testConvertorInputWithoutArguments() throws IOException, InvalidArgumentException {
+       String input = inputDate+",14,add";
        Convertor convertor = new ConvertorImpl();
        convertor.convert(input);
     }
+    
+    @Test(expected=DateTimeParseException
+            .class)
+    public void testConvertorInputWithoutArguma() throws IOException, InvalidArgumentException {
+       String input = "2016-12-11"+",14,add,incapsulate,run";
+       Convertor convertor = new ConvertorImpl();
+       convertor.convert(input);
+    }
+    
+    
+    
+    
 
 }
