bda22abc228b85affa8d0c241553b2a45e49afc3
Ramiz <abadulla@live.ru>
2016-02-08 20:32:38 +0200

    added test for reducer as well

--- src/test/java/com/segmentreader/mapreduce/AbstractCookieReducerTestCase.java
+++ src/test/java/com/segmentreader/mapreduce/AbstractCookieReducerTestCase.java
@@ -5,12 +5,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
+import com.google.common.collect.Lists;
+import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Reducer.Context;
@@ -18,8 +21,11 @@ import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 import com.segmentreader.useroperations.OperationHandler;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 
 public class AbstractCookieReducerTestCase {
+
 
     private AbstractCookieReducer createInstance (Map<String, OperationHandler> handlers) {
         
@@ -34,24 +40,29 @@ public class AbstractCookieReducerTestCase {
     @Test
     public void testAbstractCookieReducer() throws IOException, InterruptedException {
         OperationHandler handler = mock(OperationHandler.class);
+        ArgumentCaptor<UserModCommand> umcCaptor = ArgumentCaptor.forClass(UserModCommand.class);
         Counter mapRedCounter = mock(Counter.class);
         Instant timeStamp = Instant.EPOCH;
         Map<String, OperationHandler> handlers = ImmutableMap.of(OperationHandler.DELETE_OPERATION, handler,
                 OperationHandler.ADD_OPERATION, handler);
-        UserModCommand umc = new UserModCommand(timeStamp,"11",OperationHandler.ADD_OPERATION, 
-                Arrays.asList("iphone", "macbook", "magic mouse"));
-        UserModCommand umc1 = new UserModCommand(timeStamp,"11",OperationHandler.DELETE_OPERATION, 
+        List<String> umc1Segments = Arrays.asList("iphone", "macbook", "magic mouse");
+        Collections.reverse(umc1Segments);
+        List<String> umc2Segments = Arrays.asList("dakine bag", "dakine case", "dakine gloves");
+        Collections.reverse(umc2Segments);
+        UserModCommand umc = new UserModCommand(timeStamp,"11",OperationHandler.ADD_OPERATION,umc1Segments);
+        UserModCommand umc1 = new UserModCommand(timeStamp,"11",OperationHandler.DELETE_OPERATION,
                 Arrays.asList("dakine bag", "dakine case", "dakine gloves"));
-
         Iterable<UserModCommand> values = Arrays.asList(umc,umc1);
         Context context = mock(Context.class);
         
         when(context.getCounter("segmentreader", "reduce_counter")).thenReturn(mapRedCounter);
         
         AbstractCookieReducer reducer = createInstance(handlers);
-        reducer.reduce(new Text("1"), values, context);
+        reducer.reduce(new Text("11"), values, context);
  
-        verify(handler, times(2)).handle(any(UserModCommand.class));
+        verify(handler, times(2)).handle(umcCaptor.capture());
         verify(mapRedCounter, times(1)).increment(1);
+        List<UserModCommand> expected = Lists.newArrayList(values);
+        assertThat(expected, is(umcCaptor.getAllValues()));
     }
 }
