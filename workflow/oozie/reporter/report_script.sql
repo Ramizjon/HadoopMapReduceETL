402e966903db01c2f1337874c49dc2ff93129e8b
Ramiz <abadulla@live.ru>
2016-03-28 17:18:32 +0300

    implemented new feature: reporting frequency of income of every segment by date

--- workflow/oozie/reporter/report_script.sql
+++ workflow/oozie/reporter/report_script.sql
@@ -15,20 +15,20 @@ LOCATION "/user/cloudera/reporter_output/";
 
 WITH segment_counts AS (
 SELECT
-key,
-COUNT(value)
-FROM user_operations_parquet
-LATERAL VIEW EXPLODE(segmentTimestamps) pair
-AS key, value
+    key,
+    COUNT(value)
+    FROM user_operations_parquet
+    LATERAL VIEW EXPLODE(segmentTimestamps) pair
+    AS key, value
 WHERE
-year = ${year} AND
-month = ${month} AND
-day = ${day}
+    year = ${year} AND
+    month = ${month} AND
+    day = ${day}
 GROUP BY key)
 
 INSERT INTO TABLE tsv_dump
 PARTITION
  (year = ${year},
- month = ${month},
- day = ${day})
+  month = ${month},
+  day = ${day})
 SELECT * FROM segment_counts;
